98a2af129aa61f41324b0ae9141d352b2bc56ee8
hyeyoon98 <ymcacom665@gmail.com>
2021-02-22 02:37:45 +0900

    setup/retrofit

--- app/src/main/java/com/example/market/MainActivity.java
+++ app/src/main/java/com/example/market/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.market;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.market.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding.setOnClick(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
