1d81ad2daa70d3b6b16f3a0d428b699383e15bcd
hyeyoon98 <ymcacom665@gmail.com>
2021-02-22 02:47:08 +0900

    -ing

---
+++ app/src/main/java/com/example/market/RetrofitClient.java
@@ -0,0 +1,21 @@
+package com.example.market;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static final String BASE_URL = "http://여러분의 api주소/";
+
+    public static RetrofitAPI getApiService(){return getInstance().create(RetrofitAPI.class);}
+
+    private static Retrofit getInstance(){
+        Gson gson = new GsonBuilder().setLenient().create();
+        return new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+    }
+}
